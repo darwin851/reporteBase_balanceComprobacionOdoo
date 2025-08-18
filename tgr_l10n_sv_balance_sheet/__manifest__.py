a0538be2e361e80df718e6e3e7efb9e05996b3f4
darwin851 <darwin851@gmail.com>
2025-08-18 09:03:09 -0600

    C:\Users\DAG\Documents\odoo\odoo17\server\pluginSV\tgr_l10n_sv_balance_sheet

---
+++ tgr_l10n_sv_balance_sheet/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Balance General SV FUNPRES",
+    "version": "17.0.1.0.1",
+    "summary": """
+     """,
+    "countries": ["sv"],
+    "category": "Accounting/Localizations",
+    "author": "Darwin González",
+    "website": "",
+    "depends": [
+        'base',
+        'tgr_reports_base',
+        'web',
+        "account",
+        'l10n_sv',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/account_balance_sheet_views.xml',
+        'reports/balance_sheet.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'tgr_l10n_sv_balance_sheet/static/src/xml/account_balance_sheet.xml',
+            'tgr_l10n_sv_balance_sheet/static/src/js/account_balance_sheet.js',
+        ]
+    },
+    'application': True,
+    'installable': True,
+    'auto_install': False
+
+}
